2fd8602cf087f1c08d5a45ca2e45d8af381c0855
Bruno Basto <bruno.basto@liferay.com>
2017-09-19 15:52:32 -0300

    Step 01 - Boilerplate

---
+++ modules/blogs/blogs-web/src/main/java/blogs/web/portlet/BlogsWebPortlet.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package blogs.web.portlet;
+
+import blogs.web.constants.BlogsWebPortletKeys;
+
+import com.liferay.portal.portlet.bridge.soy.SoyPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Bruno Basto
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.css-class-wrapper=devcon-blogs-portlet",
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.header-portlet-css=/css/main.css",
+		"com.liferay.portlet.instanceable=false",
+		"com.liferay.portlet.single-page-application=false",
+		"javax.portlet.display-name=Devcon Blogs Portlet",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=View",
+		"javax.portlet.name=" + BlogsWebPortletKeys.BlogsWeb,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class BlogsWebPortlet extends SoyPortlet {
+}
